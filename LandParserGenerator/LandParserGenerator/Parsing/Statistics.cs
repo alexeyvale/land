3c2b7b9ff487fdeeafb53625688a9d2c77dc15d8
alexeyvale <alexeyvale.soc@yandex.ru>
2018-07-05 11:38:12 +0300

    Потерявшийся файл + модельная грамматика, иллюстрирующая проблему последовательно идущих Any

---
+++ LandParserGenerator/LandParserGenerator/Parsing/Statistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+using LandParserGenerator.Parsing.Tree;
+using LandParserGenerator.Lexing;
+
+namespace LandParserGenerator.Parsing
+{
+	public class Statistics
+	{
+		public TimeSpan TimeSpent { get; set; }
+	}
+}
