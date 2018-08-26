b347c9452302a36ada06b8a4768d2663740fb884
alexeyvale <alexeyvale.soc@yandex.ru>
2018-08-26 17:49:28 +0300

    Конвертер для получения флага активности точки разметки

---
+++ LandParserGenerator/Control/ConcernPointIsEnabledConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+using Land.Core.Markup;
+
+namespace Land.Control
+{
+	public class ConcernPointIsEnabledConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return (value is ConcernPoint)
+				? ((ConcernPoint)value).TreeNode != null
+				: true;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
