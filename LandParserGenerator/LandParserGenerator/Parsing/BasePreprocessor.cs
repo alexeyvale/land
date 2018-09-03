0d79d7e1349f6877587637229aad486b61b6dce1
alexeyvale <alexeyvale.soc@yandex.ru>
2018-09-03 08:55:15 +0300

    Препроцессинг: начало

---
+++ LandParserGenerator/LandParserGenerator/Parsing/BasePreprocessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Land.Core.Lexing;
+using Land.Core.Parsing.Tree;
+
+namespace Land.Core.Parsing
+{
+	public abstract class BasePreprocessor
+	{
+		public abstract string Preprocess(string text);
+
+		public abstract void Postprocess(Node root, List<Message> log);
+	}
+
+	public class NoopPreprocessor: BasePreprocessor
+	{
+		public override string Preprocess(string source)
+		{
+			return source;
+		}
+
+		public override void Postprocess(Node root, List<Message> log) { }
+	}
+
+	public class PipelinePreprocessor : BasePreprocessor
+	{
+		private List<BasePreprocessor> Preprocs { get; set; } = new List<BasePreprocessor>();
+
+		public PipelinePreprocessor(params BasePreprocessor[] preprocs)
+		{
+			foreach (var preproc in preprocs)
+				Preprocs.Add(preproc);
+		}
+
+		public override string Preprocess(string source)
+		{
+			foreach (var preproc in Preprocs)
+				source = preproc.Preprocess(source);
+
+			return source;
+		}
+
+		public override void Postprocess(Node root, List<Message> log)
+		{
+			foreach (var preproc in Preprocs)
+				 preproc.Postprocess(root, log);
+		}
+	}
+}
---
+++ LandParserGenerator/SharpPreprocessor/SharpPreprocessor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Land.Core;
+using Land.Core.Parsing;
+using Land.Core.Parsing.Tree;
+
+using sharp_preprocessor;
+
+namespace SharpPreprocessor
+{
+    public class SharpPreprocessor: BasePreprocessor
+    {
+		public class DirectivesVisitor: BaseTreeVisitor
+		{
+			public List<int> LinesToComment = new List<int>();
+
+			public HashSet<string> SymbolsDefined = new HashSet<string>();
+
+			public override void Visit(Node node)
+			{
+				switch(node.Symbol)
+				{
+					case "if":
+						
+						break;
+					case "define":
+						for (var i = 1; i < node.Children.Count; ++i)
+							SymbolsDefined.Add(node.Children[i].Symbol);
+						break;
+					case "undef":
+						for (var i = 1; i < node.Children.Count; ++i)
+							SymbolsDefined.Remove(node.Children[i].Symbol);
+						break;
+				}
+			}
+		}
+
+		private BaseParser Parser { get; set; }
+		public List<Message> Log { get { return Parser?.Log; } }
+
+		public SharpPreprocessor()
+		{
+			Parser = ParserProvider.GetParser();
+		}
+
+		public override string Preprocess(string text)
+		{
+			var root = Parser.Parse(text);
+
+			foreach (var rec in Log)
+				rec.Source = "Preprocessor";
+
+			return text;
+		}
+
+		public override void Postprocess(Node root, List<Message> log)
+		{
+			
+		}
+	}
+}
