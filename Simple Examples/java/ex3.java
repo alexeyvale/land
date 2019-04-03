ed47c57d98f8f61d357a086143fbd332ba3f9c2e
alexeyvale <alexeyvale.soc@yandex.ru>
2019-04-04 00:11:33 +0300

    Откатил изменения в Location-е токена из-за некорректности в случае, если последний  (или последние) символы лексемы - переход на новую строку; откатил правки Java-грамматики, так как больше теряем, чем находим

--- Simple Examples/java/ex3.java
+++ Simple Examples/java/ex3.java
@@ -4,6 +4,13 @@ public class DOMConfigurationImpl extends ParserConfigurationSettings
     protected final static short NAMESPACES          = 0x1<<0;
     protected final static short DTNORMALIZATION     = 0x1<<1;
     protected final static short ENTITIES            = 0x1<<2;
+    
+    private final static int
+        CREATE_MASK = 1<<CREATE,
+        FIND_MASK = 1<<FIND,
+        NEW_MASK = 1<<NEW,
+        RELEASE_MASK = 1<<RELEASE,
+        ALL_MASK = CREATE_MASK|FIND_MASK|NEW_MASK|RELEASE_MASK;
 
     protected final static short INFOSET_TRUE_PARAMS = NAMESPACES;
     
